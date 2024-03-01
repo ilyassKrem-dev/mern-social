1560aec1261bcc5ef5a6ee44a1f0b49be03e7af8
Ilyass <kreilyass@gmail.com>
2024-03-01 16:43:07 +0100

    fixes

--- src/assets/other/ContentImages.tsx
+++ src/assets/other/ContentImages.tsx
@@ -40,26 +40,26 @@ export default function ContentImages({images}:{images:string[]}) {
             })}
             {enlargedImage && (
             <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 flex justify-center items-center bottom-0 right-0 z-50 no-doc-scroll">
-            <div className=" p-4 rounded-lg w-[80%] sm:w-[60%] sm:h-[70%] flex flex-col gap-2 items-center justify-center relative background">
-                <button className=" hover:bg-gray-400 hover:bg-opacity-40  text-light-1 transition-all duration-300 text-heading4-medium px-3 py-1 rounded-full absolute -top-10 sm:-top-16 left-0" onClick={handleClose}>
-                    X
-                </button>
-                <Image src={enlargedImage} width={600} height={600} alt="Enlarged" className="max-w-full max-h-full rounded-lg " />
-                <div className="flex mt-2">
-                    {images.map((image, index) => (
-                        <div className="w-[90px] h-[90px] mr-2" key={index}>
-                            <Image 
-                                src={image} 
-                                alt={`image ${index}`} 
-                                width={200} 
-                                height={200} 
-                                className="w-full h-full object-cover cursor-pointer hover:opacity-50 transition-all duration-200 rounded-xl"
-                                onClick={() => setEnlargedImage(image)}
-                            />
-                        </div>
-                    ))}
+                <div className=" p-4 rounded-lg w-[80%] sm:w-[60%] sm:h-[70%] flex flex-col gap-2 items-center justify-center relative background">
+                    <button className=" hover:bg-gray-400 hover:bg-opacity-40  text-light-1 transition-all duration-300 text-heading4-medium px-3 py-1 rounded-full absolute -top-10 sm:-top-16 left-0" onClick={handleClose}>
+                        X
+                    </button>
+                    <Image src={enlargedImage} width={600} height={600} alt="Enlarged" className="max-w-full max-h-full rounded-lg " />
+                    <div className="flex mt-2">
+                        {images.map((image, index) => (
+                            <div className="w-[90px] h-[90px] mr-2" key={index}>
+                                <Image 
+                                    src={image} 
+                                    alt={`image ${index}`} 
+                                    width={200} 
+                                    height={200} 
+                                    className="w-full h-full object-cover cursor-pointer hover:opacity-50 transition-all duration-200 rounded-xl"
+                                    onClick={() => setEnlargedImage(image)}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
             </div>
         )}
         </div>
